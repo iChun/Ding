522876cd58154a33a5c53a5b221cde4f12fd0fd9
iChun <ohai.iChun@gmail.com>
2019-10-04 05:59:48 +0800

    Update build.gradle. Sign Jar. Fix logging.

--- src/main/java/me/ichun/mods/ding/common/core/Ding.java
+++ src/main/java/me/ichun/mods/ding/common/core/Ding.java
@@ -8,10 +8,10 @@ import net.minecraft.util.SoundEvent;
 import net.minecraftforge.client.event.GuiOpenEvent;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.common.config.Configuration;
-import net.minecraftforge.fml.common.FMLLog;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.Mod.Instance;
+import net.minecraftforge.fml.common.event.FMLFingerprintViolationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.eventhandler.EventPriority;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
@@ -24,6 +24,7 @@ import org.apache.logging.log4j.Logger;
 
 @Mod(modid = "ding", name = "Ding",
         version = Ding.VERSION,
+        certificateFingerprint = Ding.CERT_FINGERPRINT,
         clientSideOnly = true,
         acceptableRemoteVersions = "*",
         dependencies = "required-after:forge@[13.19.0.2141,)",
@@ -32,6 +33,8 @@ import org.apache.logging.log4j.Logger;
 public class Ding
 {
     public static final String VERSION = "1.0.2";
+
+    public static final String CERT_FINGERPRINT = "4db5c2bd1b556f252a5b8b54b256d381b2a0a6b8";
 
     public static String name = "entity.experience_orb.pickup";
     public static double pitch = 1.0D;
@@ -57,10 +60,10 @@ public class Ding
         Configuration config = new Configuration(event.getSuggestedConfigurationFile());
         config.load();
 
-        name = config.getString("name", "ding", name, "Minecraft name of sound file to play when Minecraft is loaded\nEG: \"ui.button.click\" or \"entity.experience_orb.pickup\"\n\n" + "This can also be a mod sound if the mod is installed.\nEG: modname:modsound.boing", "entity.experience_orb.pickup");
+        name = config.getString("name", "ding", name, "Minecraft name of sound file to play when Minecraft is loaded\nEG: \"ui.button.click\" or \"entity.experience_orb.pickup\"\n\n" + "This can also be a mod sound if the mod is installed.\nEG: modname:modsound.boing\n\nIf you want to use external sounds, consider looking into the mod Additional Resources\n", "entity.experience_orb.pickup");
         pitch = (double)config.getFloat("pitch", "ding", (float)pitch, 0F, 10F, "Pitch of the sound to play when Minecraft is loaded");
 
-        nameWorld = config.getString("nameWorld", "ding", nameWorld, "Minecraft name of sound file to play when world is loaded\nEG: \"ui.button.click\" or \"entity.experience_orb.pickup\"\n\n" + "This can also be a mod sound if the mod is installed.\nEG: modname:modsound.boing", "entity.experience_orb.pickup");
+        nameWorld = config.getString("nameWorld", "ding", nameWorld, "Minecraft name of sound file to play when world is loaded\nEG: \"ui.button.click\" or \"entity.experience_orb.pickup\"\n\n" + "This can also be a mod sound if the mod is installed.\nEG: modname:modsound.boing\n\nIf you want to use external sounds, consider looking into the mod Additional Resources\n", "entity.experience_orb.pickup");
         pitchWorld = (double)config.getFloat("pitchWorld", "ding", (float)pitchWorld, 0F, 10F, "Pitch of the sound to play when world is loaded");
 
         playOn = config.getInt("playOn", "ding", playOn, 0, 3, "Play sound on...\n0 = Nothing (why install the mod though?)\n1 = MC load\n2 = World load\n3 = MC and World load");
@@ -71,6 +74,15 @@ public class Ding
         }
 
         MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    @EventHandler
+    public void onFingerprintViolation(FMLFingerprintViolationEvent event)
+    {
+        if(event.getSource() != null && event.getSource().isFile())
+        {
+            logger.warn("The file " + event.getSource().getName() + " has been modified. Support for the mod will not be provided.");
+        }
     }
 
     @SubscribeEvent(priority = EventPriority.LOWEST)
@@ -89,7 +101,7 @@ public class Ding
                 }
                 else
                 {
-                    logger.log(Level.WARN, "Could not find sound: %s", new ResourceLocation(name));
+                    logger.log(Level.WARN, "Could not find sound: {}", new ResourceLocation(name));
                 }
             }
         }
@@ -118,7 +130,7 @@ public class Ding
                 }
                 else
                 {
-                    logger.log(Level.WARN, "Could not find sound: %s", new ResourceLocation(nameWorld));
+                    logger.log(Level.WARN, "Could not find sound: {}", new ResourceLocation(nameWorld));
                 }
             }
         }
