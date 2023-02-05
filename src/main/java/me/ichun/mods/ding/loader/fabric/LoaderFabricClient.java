4c5887ff851a0ee092c9fe8edcb3111f94466b96
iChun <ohai.iChun@gmail.com>
2023-02-05 01:26:37 +0000

    Backport 1.19.3+Fabric to 1.19.2.

---
+++ src/main/java/me/ichun/mods/ding/loader/fabric/LoaderFabricClient.java
@@ -0,0 +1,25 @@
+package me.ichun.mods.ding.loader.fabric;
+
+import me.ichun.mods.ding.common.Ding;
+import me.lortseam.completeconfig.data.Config;
+import net.fabricmc.api.ClientModInitializer;
+
+public class LoaderFabricClient extends Ding
+    implements ClientModInitializer
+{
+    @Override
+    public void onInitializeClient()
+    {
+        modProxy = this;
+
+        //register config
+        ConfigFabric configFabric = new ConfigFabric();
+        config = configFabric;
+        Config modConfig = new Config(MOD_ID, new String[]{}, configFabric);
+        modConfig.load();
+        Runtime.getRuntime().addShutdownHook(new Thread(modConfig::save));
+
+        //init event handler
+        new EventHandlerClientFabric();
+    }
+}
