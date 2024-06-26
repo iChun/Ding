309ecce0eadb27a8e59f29f57011a0935e138f0c
iChun <ohai.iChun@gmail.com>
2024-06-26 03:19:57 +0100

    Backport 1.20.6 to 1.20.4.

--- neoforge/src/main/java/me/ichun/mods/ding/loader/neoforge/LoaderNeoForge.java
+++ neoforge/src/main/java/me/ichun/mods/ding/loader/neoforge/LoaderNeoForge.java
@@ -10,7 +10,7 @@ import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.ModLoadingContext;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.fml.loading.FMLEnvironment;
-import net.neoforged.neoforge.client.gui.IConfigScreenFactory;
+import net.neoforged.neoforge.client.ConfigScreenHandler;
 
 @Mod(Ding.MOD_ID)
 public class LoaderNeoForge extends Ding
@@ -36,6 +36,6 @@ public class LoaderNeoForge extends Ding
 
         new EventHandlerClientNeoForge();
 
-        ModLoadingContext.get().registerExtensionPoint(IConfigScreenFactory.class, () -> (minecraft, screen) -> new WorkspaceConfigs(screen));
+        ModLoadingContext.get().registerExtensionPoint(ConfigScreenHandler.ConfigScreenFactory.class, () -> new ConfigScreenHandler.ConfigScreenFactory((mc, screen) -> new WorkspaceConfigs(screen)));
     }
 }
