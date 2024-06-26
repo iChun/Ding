dc853c703b61d8879d29618ee0603a5802d1116b
iChun <ohai.iChun@gmail.com>
2024-06-26 04:49:10 +0100

    Backport 1.20.4 to 1.20.2

--- forge/src/main/java/me/ichun/mods/ding/loader/forge/LoaderForge.java
+++ forge/src/main/java/me/ichun/mods/ding/loader/forge/LoaderForge.java
@@ -23,7 +23,7 @@ public class LoaderForge extends Ding
         DistExecutor.unsafeRunWhenOn(Dist.DEDICATED_SERVER, () -> () -> LOGGER.error("You are loading " + MOD_NAME + " on a server. " + MOD_NAME + " is a client only mod!"));
 
         //Make sure the mod being absent on the other network side does not cause the client to display the server as incompatible
-        ModLoadingContext.get().registerExtensionPoint(IExtensionPoint.DisplayTest.class, IExtensionPoint.DisplayTest.IGNORE_ALL_VERSION);
+        ModLoadingContext.get().registerExtensionPoint(IExtensionPoint.DisplayTest.class, () -> new IExtensionPoint.DisplayTest(() -> "", (remoteVersion, isFromServer) -> true));
     }
 
     @OnlyIn(Dist.CLIENT)
@@ -33,6 +33,6 @@ public class LoaderForge extends Ding
 
         new EventHandlerClientForge();
 
-        ModLoadingContext.get().registerExtensionPoint(ConfigScreenHandler.ConfigScreenFactory.class, () -> new ConfigScreenHandler.ConfigScreenFactory(WorkspaceConfigs::new));
+        ModLoadingContext.get().registerExtensionPoint(ConfigScreenHandler.ConfigScreenFactory.class, () -> new ConfigScreenHandler.ConfigScreenFactory((mc, screen) -> new WorkspaceConfigs(screen)));
     }
 }
