db827566f28ea731009e5c86303388163c5a4086
iChun <ohai.iChun@gmail.com>
2024-07-28 09:58:37 +0100

    Add dist to @Mod to mark the mod as client-only.

--- neoforge/src/main/java/me/ichun/mods/ding/loader/neoforge/LoaderNeoForge.java
+++ neoforge/src/main/java/me/ichun/mods/ding/loader/neoforge/LoaderNeoForge.java
@@ -12,7 +12,7 @@ import net.neoforged.fml.common.Mod;
 import net.neoforged.fml.loading.FMLEnvironment;
 import net.neoforged.neoforge.client.gui.IConfigScreenFactory;
 
-@Mod(Ding.MOD_ID)
+@Mod(value = Ding.MOD_ID, dist = Dist.CLIENT)
 public class LoaderNeoForge extends Ding
 {
     public LoaderNeoForge(IEventBus modEventBus)
