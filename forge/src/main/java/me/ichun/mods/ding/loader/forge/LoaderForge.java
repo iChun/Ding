bf0add924052057c6b7ac37310b368322fd5e4c5
iChun <ohai.iChun@gmail.com>
2024-12-28 13:32:02 +0000

    Update Forge loader classes to include the ModLoadingContext for extension point registration, config registration and mod IEventBus access. Update NeoForge loader classes to include the ModContainer for extension point registration. Forge configs now crash if FMLJavaModLoadingContext is not provided as the first param. In my infinite wisdom I will not be testing this. Properly generify a large proportion of B&S classes. This was a headache. ElementClickable can now handle right clicks natively. Opening the mod's config editor actually opens the editor to the selected mod.

--- forge/src/main/java/me/ichun/mods/ding/loader/forge/LoaderForge.java
+++ forge/src/main/java/me/ichun/mods/ding/loader/forge/LoaderForge.java
@@ -9,30 +9,30 @@ import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.client.ConfigScreenHandler;
 import net.minecraftforge.fml.DistExecutor;
 import net.minecraftforge.fml.IExtensionPoint;
-import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 @Mod(Ding.MOD_ID)
 public class LoaderForge extends Ding
 {
-    public LoaderForge()
+    public LoaderForge(FMLJavaModLoadingContext context)
     {
         modProxy = this;
 
-        DistExecutor.unsafeRunWhenOn(Dist.CLIENT, () -> this::initClient);
+        DistExecutor.unsafeRunWhenOn(Dist.CLIENT, () -> () -> initClient(context));
         DistExecutor.unsafeRunWhenOn(Dist.DEDICATED_SERVER, () -> () -> LOGGER.error("You are loading " + MOD_NAME + " on a server. " + MOD_NAME + " is a client only mod!"));
 
         //Make sure the mod being absent on the other network side does not cause the client to display the server as incompatible
-        ModLoadingContext.get().registerExtensionPoint(IExtensionPoint.DisplayTest.class, IExtensionPoint.DisplayTest.IGNORE_ALL_VERSION);
+        context.registerExtensionPoint(IExtensionPoint.DisplayTest.class, IExtensionPoint.DisplayTest.IGNORE_ALL_VERSION);
     }
 
     @OnlyIn(Dist.CLIENT)
-    private void initClient()
+    private void initClient(FMLJavaModLoadingContext context)
     {
-        Ding.config = iChunUtil.d().registerConfig(new Config());
+        config = iChunUtil.d().registerConfig(new Config(), context);
 
         new EventHandlerClientForge();
 
-        ModLoadingContext.get().registerExtensionPoint(ConfigScreenHandler.ConfigScreenFactory.class, () -> new ConfigScreenHandler.ConfigScreenFactory(WorkspaceConfigs::new));
+        context.registerExtensionPoint(ConfigScreenHandler.ConfigScreenFactory.class, () -> new ConfigScreenHandler.ConfigScreenFactory((mc, screen) -> new WorkspaceConfigs(screen, MOD_ID)));
     }
 }
--- neoforge/src/main/java/me/ichun/mods/ding/loader/neoforge/LoaderNeoForge.java
+++ neoforge/src/main/java/me/ichun/mods/ding/loader/neoforge/LoaderNeoForge.java
@@ -7,21 +7,23 @@ import me.ichun.mods.ichunutil.common.iChunUtil;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.api.distmarker.OnlyIn;
 import net.neoforged.bus.api.IEventBus;
-import net.neoforged.fml.ModLoadingContext;
+import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.fml.loading.FMLEnvironment;
 import net.neoforged.neoforge.client.gui.IConfigScreenFactory;
 
+import java.util.function.Supplier;
+
 @Mod(value = Ding.MOD_ID, dist = Dist.CLIENT)
 public class LoaderNeoForge extends Ding
 {
-    public LoaderNeoForge(IEventBus modEventBus)
+    public LoaderNeoForge(IEventBus modEventBus, ModContainer container)
     {
         modProxy = this;
 
         if(FMLEnvironment.dist.isClient())
         {
-            initClient(modEventBus);
+            initClient(modEventBus, container);
         }
         else
         {
@@ -30,12 +32,12 @@ public class LoaderNeoForge extends Ding
     }
 
     @OnlyIn(Dist.CLIENT)
-    private void initClient(IEventBus modEventBus)
+    private void initClient(IEventBus modEventBus, ModContainer container)
     {
         Ding.config = iChunUtil.d().registerConfig(new Config(), modEventBus);
 
         new EventHandlerClientNeoForge();
 
-        ModLoadingContext.get().registerExtensionPoint(IConfigScreenFactory.class, () -> (modContainer, screen) -> new WorkspaceConfigs(screen));
+        container.registerExtensionPoint(IConfigScreenFactory.class, (Supplier<IConfigScreenFactory>)() -> (modContainer, screen) -> new WorkspaceConfigs(screen, MOD_ID));
     }
 }
